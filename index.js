9adc62cd7c3949101a3578fdf5a8272715d0b052
Elijah Woodhouse <elijahwoodhouse95@gmail.com>
2022-06-10 00:35:24 -0600

    task1 completed

--- index.js
+++ index.js
@@ -1,16 +1,17 @@
-//❗❗ PLEASE READ THE README file for project instructions, helpful resources, additional tasks and stretch problems, and more ❗❗ 
+//❗❗ PLEASE READ THE README file for project instructions, helpful resources,
+// additional tasks and stretch problems, and more ❗❗
 
 // ⭐️ Example Challenge START ⭐️
 
 /**Example Task : processFirstItem()
  * This example shows how you might go about solving the rest of the tasks
- * 
+ *
  * Use the higher order function processFirstItem below to do the following:
  *  1. Receive an array of strings in a parameter
  *  2. Receive a callback function that takes a string as its argument in a parameter
- *  3. Return the result of invoking the callback function and passing in the FIRST 
+ *  3. Return the result of invoking the callback function and passing in the FIRST
  *     element in the array as the argument
- * 
+ *
  * The following code is demonstrating a way of completing this task
  * It returns the string `foofoo`
 */
@@ -26,15 +27,27 @@ console.log('example task:', processFirstItem(['foo','bar'],function(str){return
 ///// M V P ///////
 
 /*Task 1: counterMaker()
-  
+
   Study the code for counter1 and counter2, then answer the questions below.
-  
+
   1. What is the difference between counter1 and counter2?
-  
+  counter one has a counter variable that is only available inside the scope
+  of the function. Counter 2 has a variable that is available outside of the scope
+  of the function. So I can access the counter variable from counter 2 anywhere
+  in my code. Where as I can only access the counter 1 variable from inside that
+  specific function.
+
   2. Which of the two uses a closure? How can you tell?
-  
-  3. In what scenario would the counter1 code be preferable? In what scenario would 
-     counter2 be better?  
+  counter 1 uses closure because the count variable is available within that function.
+  the counter variable in counter2 is available for use outside of the scope
+  of the function.
+
+  3. In what scenario would the counter1 code be preferable? In what scenario would
+     counter2 be better?
+     counter1 might be preferable if you're keeping score of something or if you
+     are creating a function who's purpose is to run a loop based on a certain parameter.
+     Counter2 would be preferable if you are counting data maybe and need to reference
+     the data you just counted at a later point.
 */
 
 // counter1 code
@@ -57,42 +70,66 @@ function counter2() {
 
 /* ⚾️⚾️⚾️ Task 2: inning() ⚾️⚾️⚾️
 Use the inning function below to do the following:
-  1. Return a random whole number of points between 0 and 2 scored by one team in an inning
-  
-  For example: invoking inning() should return a numerical score value of 0, 1, or 2
-  
-NOTE: This will be a callback function for the tasks below
-*/
+  1. Return a random whole number of points between
+  0 and 2 scored by one team in an inning
 
-function inning(/*Code Here*/){
-    /*Code Here*/
+  For example: invoking inning() should return a
+  numerical score value of 0, 1, or 2
+
+NOTE: This will be a callback function for the tasks
+below
+*/
+var randomNumber = 0;
+
+function inning(rando){
+    rando = Math.floor(Math.random() * 3);
+    return rando;
 }
+
+emptyArray = [];
+
+for (let i = 0; i < 100; i++){
+  // console.log(inning(randomNumber));
+  emptyArray.push(inning(randomNumber));
+}
+console.log(emptyArray);
+console.log(randomNumber);
 
 
 /* ⚾️⚾️⚾️ Task 3: finalScore() ⚾️⚾️⚾️
 Use the finalScore function below to do the following:
-  1. Receive the callback function `inning` that was created in Task 2 in the first parameter
-  2. Receive a number of innings to be played in the second parameter
-  3. After each inning, update the score of the home and away teams
-  4. After the last inning, return an object containing the final (total) score of the innings played
-  
-  For example: invoking finalScore(inning, 9) might return this object:
+  1. Receive the callback function `inning`
+  that was created in Task 2 in the first parameter
+  2. Receive a number of innings to be played in
+  the second parameter
+  3. After each inning, update the score of the
+   home and away teams
+  4. After the last inning, return an object
+  containing the final (total) score of the innings
+  played
+
+  For example: invoking finalScore(inning, 9) might
+  return this object:
 {
   "Home": 11,
   "Away": 5
 }
-*/ 
+*/
 
-function finalScore(/*Code Here*/){
-  /*Code Here*/
+function finalScore(funkyton, num){
+  homeTeam = 0;
+  awayTeam = 0;
+  for (let i = 0; i < num; i++){
+
+  }
 }
 
 
 /* ⚾️⚾️⚾️ Task 4: getInningScore() ⚾️⚾️⚾️
 Use the getInningScore() function below to do the following:
-  1. Receive a callback function in a parameter - you will pass in the inning function from task 2 as your argument 
-  2. Return an object with a score for home and a score for away that populates from invoking the inning callback function 
-  
+  1. Receive a callback function in a parameter - you will pass in the inning function from task 2 as your argument
+  2. Return an object with a score for home and a score for away that populates from invoking the inning callback function
+
 For example: invoking getInningScore(inning) might return this object:
 {
   "Home": 0,
@@ -116,34 +153,34 @@ Use the scoreboard function below to do the following:
   Home and Away team's scores for each inning.  Not the cummulative score (see the example below).
   5. If there's a tie at the end of the innings, add this message containing the score to the end of the array:  "This game will require extra innings: Away 12 - Home 12"  (see tie example below)
      If there isn't a tie, add this message to the end of the array: "Final Score: Away 13 - Home 11"  (see no tie example below)
-  
-  NO TIE example: invoking scoreboard(getInningScore,inning, 9) might return 
+
+  NO TIE example: invoking scoreboard(getInningScore,inning, 9) might return
   an array of strings like this:
 [
-  "Inning 1: Away 1 - Home 2", 
+  "Inning 1: Away 1 - Home 2",
   "Inning 2: Away 2 - Home 1",
-  "Inning 3: Away 0 - Home 2", 
-  "Inning 4: Away 2 - Home 2", 
-  "Inning 5: Away 2 - Home 0", 
-  "Inning 6: Away 1 - Home 1", 
-  "Inning 7: Away 0 - Home 2", 
+  "Inning 3: Away 0 - Home 2",
+  "Inning 4: Away 2 - Home 2",
+  "Inning 5: Away 2 - Home 0",
+  "Inning 6: Away 1 - Home 1",
+  "Inning 7: Away 0 - Home 2",
   "Inning 8: Away 2 - Home 2",
-  "Inning 9: Away 1 - Home 0", 
-  "Final Score: Away 11 - Home 12"  
+  "Inning 9: Away 1 - Home 0",
+  "Final Score: Away 11 - Home 12"
 ]
 
-  TIE example: invoking scoreboard(getInningScore,inning, 9) might return 
+  TIE example: invoking scoreboard(getInningScore,inning, 9) might return
   an array of strings like this:
 [
-  "Inning 1: Away 1 - Home 1", 
+  "Inning 1: Away 1 - Home 1",
   "Inning 2: Away 2 - Home 2",
-  "Inning 3: Away 1 - Home 0", 
-  "Inning 4: Away 1 - Home 2", 
-  "Inning 5: Away 0 - Home 0", 
-  "Inning 6: Away 2 - Home 1", 
-  "Inning 7: Away 0 - Home 2", 
+  "Inning 3: Away 1 - Home 0",
+  "Inning 4: Away 1 - Home 2",
+  "Inning 5: Away 0 - Home 0",
+  "Inning 6: Away 2 - Home 1",
+  "Inning 7: Away 0 - Home 2",
   "Inning 8: Away 2 - Home 1",
-  "Inning 9: Away 1 - Home 1", 
+  "Inning 9: Away 1 - Home 1",
   "This game will require extra innings: Away 10 - Home 10"
 ] */
 // NOTE: There is no test associated with this code; if your output matches the given example, consider it complete!
